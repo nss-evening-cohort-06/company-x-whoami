b7db8629075fcd762eebc1b8178a1311da99dbd3
holmanhub <holman.eric85@gmail.com>
2017-08-08 19:18:52 -0500

    added product arrays

---
+++ shop.js
@@ -0,0 +1,97 @@
+var allProducts = [];
+
+var product1 = {
+  name: "product name",
+  imagePath: "",
+  imageAlt: "",
+  price: 0,
+  soldOut: false 
+};
+
+var product2 = {
+  name: "product name",
+  imagePath: "",
+  imageAlt: "",
+  price: 0,
+  soldOut: false 
+};
+
+var product3 = {
+  name: "product name",
+  imagePath: "",
+  imageAlt: "",
+  price: 0,
+  soldOut: false 
+};
+
+var product4 = {
+  name: "product name",
+  imagePath: "",
+  imageAlt: "",
+  price: 0,
+  soldOut: false 
+};
+
+var product5 = {
+  name: "product name",
+  imagePath: "",
+  imageAlt: "",
+  price: 0,
+  soldOut: false 
+};
+
+var product6 = {
+  name: "product name",
+  imagePath: "",
+  imageAlt: "",
+  price: 0,
+  soldOut: false 
+};
+
+var product7 = {
+  name: "product name",
+  imagePath: "",
+  imageAlt: "",
+  price: 0,
+  soldOut: false 
+};
+
+var product8 = {
+  name: "product name",
+  imagePath: "",
+  imageAlt: "",
+  price: 0,
+  soldOut: false 
+};
+
+var product9 = {
+  name: "product name",
+  imagePath: "",
+  imageAlt: "",
+  price: 0,
+  soldOut: false 
+};
+
+var product10 = {
+  name: "product name",
+  imagePath: "",
+  imageAlt: "",
+  price: 0,
+  soldOut: false 
+};
+
+var product11 = {
+  name: "product name",
+  imagePath: "",
+  imageAlt: "",
+  price: 0,
+  soldOut: false 
+};
+
+var product12 = {
+  name: "product name",
+  imagePath: "",
+  imageAlt: "",
+  price: 0,
+  soldOut: false 
+};
