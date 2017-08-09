14dec9bd64793d75866781b7293172e5c63aceec
holmanhub <holman.eric85@gmail.com>
2017-08-08 19:43:16 -0500

    added DOMstring for product page

--- shop.js
+++ shop.js
@@ -83,3 +83,37 @@ var product12 = {
   imageAlt: "",
   price: 0
 };
+
+allProducts.push(product1);
+allProducts.push(product2);
+allProducts.push(product3);
+allProducts.push(product4);
+allProducts.push(product5);
+allProducts.push(product6);
+allProducts.push(product7);
+allProducts.push(product8);
+allProducts.push(product9);
+allProducts.push(product10);
+allProducts.push(product11);
+allProducts.push(product12);
+
+console.log("FGBG Product Line: ", allProducts);
+
+var productContainer = document.getElementById("product-container");
+
+
+function buildDomString(product){
+  var domString = "";
+
+  domString +=  '<section class="product">';
+    domString +=    '<div class="title">';
+    domString +=          '<h2>' + product.name + '</h2>';
+    domString +=      '</div>';
+    domString +=      '<div class="image">';
+    domString +=      '<img src="' + product.imagePath + '" alt="' + product.imageAlt + '">';
+    domString +=    '</div>';
+    domString +=      '<h6>$' + product.price + '</h6>';
+    domString +=    '</div>';
+    domString +=    '</section>';
+    return domString;
+}
