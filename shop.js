57e9f709a51c2fd3ba52bbd74e2d00c290abc693
holmanhub <holman.eric85@gmail.com>
2017-08-08 19:29:01 -0500

    delete comma after price

--- shop.js
+++ shop.js
@@ -4,82 +4,82 @@ var product1 = {
   name: "product name",
   imagePath: "",
   imageAlt: "",
-  price: 0,
+  price: 0
 };
 
 var product2 = {
   name: "product name",
   imagePath: "",
   imageAlt: "",
-  price: 0, 
+  price: 0
 };
 
 var product3 = {
   name: "product name",
   imagePath: "",
   imageAlt: "",
-  price: 0,
+  price: 0
 };
 
 var product4 = {
   name: "product name",
   imagePath: "",
   imageAlt: "",
-  price: 0,
+  price: 0
 };
 
 var product5 = {
   name: "product name",
   imagePath: "",
   imageAlt: "",
-  price: 0,
+  price: 0
 };
 
 var product6 = {
   name: "product name",
   imagePath: "",
   imageAlt: "",
-  price: 0,
+  price: 0
 };
 
 var product7 = {
   name: "product name",
   imagePath: "",
   imageAlt: "",
-  price: 0,
+  price: 0
 };
 
 var product8 = {
   name: "product name",
   imagePath: "",
   imageAlt: "",
-  price: 0,
+  price: 0
 };
 
 var product9 = {
   name: "product name",
   imagePath: "",
   imageAlt: "",
-  price: 0, 
+  price: 0 
 };
 
 var product10 = {
   name: "product name",
   imagePath: "",
   imageAlt: "",
-  price: 0,
+  price: 0
 };
 
 var product11 = {
   name: "product name",
   imagePath: "",
   imageAlt: "",
-  price: 0,
+  price: 0
 };
 
 var product12 = {
   name: "product name",
   imagePath: "",
   imageAlt: "",
-  price: 0,
+  price: 0
 };
